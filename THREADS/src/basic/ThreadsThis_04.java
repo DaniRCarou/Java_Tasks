89c73c91b183b14d3b4a505fe59d657051406eb1
DaniRCarou <danicarou.dev@gmail.com>
2023-10-21 19:13:54 +0200

    ThreadsThis_04 added

---
+++ THREADS/src/basic/ThreadsThis_04.java
@@ -0,0 +1,86 @@
+package basic;
+
+
+
+//"Threads" objects can be created and started automatically (see constructor of ThreadsThis_04 class)
+
+
+
+
+public class ThreadsThis_04 implements Runnable {
+
+	
+	
+	
+	
+	// Variable whose value is entered through the constructor
+	
+	private String printValue;
+	
+	
+	
+	
+	
+	
+
+	// Constructor
+	
+	public ThreadsThis_04(String printValue, String threadName) {
+		
+		this.printValue = printValue;
+		
+		Thread t1 = new Thread(this,threadName);    // This variable will contain the Runnable object from this, this object
+		
+		t1.start();									// it starts the thread t1
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	// You have to implement the run method, because Runnable is an interface and doesn´t have functionality 																	| Debemos sobreescribir el método run
+	// The run method is like an independent thread, whose duration will be the duration of the method itself. 	| Tenemos que entender el método run, como un hilo independiente, cuya duración será la duración del propio método.
+				
+	@Override
+	public void run() {
+		
+		
+		System.out.println("Starting thread: " + Thread.currentThread().getName());  
+		
+		
+		
+		
+		for (int i = 0; i < 20; i++) {
+			
+
+			try {
+				
+				Thread.sleep(500); // Simulation of task that sleeps the thread for 500 milliseconds 							| Simulación de tarea que duerme el hilo durante 500 milisegundos
+				
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
+
+			System.out.println("Thread: " + Thread.currentThread().getName() + " , says: " + printValue + " cycle: " + i);
+			
+			
+		}
+		
+		
+		
+		
+		System.out.println("Thread: " + Thread.currentThread().getName() + " finished");
+			
+	
+	}
+		
+		
+		
+}
---
+++ THREADS/src/main/Main_ThreadsThis_04.java
@@ -0,0 +1,27 @@
+package main;
+
+import basic.ThreadsThis_04;
+
+public class Main_ThreadsThis_04 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// "Threads" objects can be created and started automatically (see constructor of ThreadsThis_04 class)
+		
+		new ThreadsThis_04("Value1", "Thread1");
+		
+		new ThreadsThis_04("Value2", "Thread2");
+		
+		
+		
+		
+		System.out.println("Main thread finishing");
+		
+		
+		
+		
+	}
+
+}
